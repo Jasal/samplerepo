3f35c4e8e45a60f01ebc7b18c7f60d44488b5ef2
developer-jasal <jasalasus@gmail.com>
2020-06-07 14:08:23 +0530

    uploading to heroko

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+/*
 var express=require('express')
 var bodypaser=require('body-parser');
 
@@ -26,11 +28,70 @@ server.get("/services",function(req,res){
     res.render("services")
 })
 server.post("/validate",function(req,res){
-    console.log(req.body.user)
-    if(req.body.user=="admin"){
-        res.send("login success")
+    var user=req.body.userid;
+    console.log(user)
+    var password=req.body.pass;
+    console.log(password)
+    if(user=="admin"&&password=="pass@123"){
+        res.render("home",{students:data})
     }
     else{
-        res.send("login failed")
+        res.render("login")
     }
 })
+
+
+    
+
+*/
+
+var express=require('express');
+var bodyparser=require("body-parser")
+const server=express();
+var data=[
+    {Name:"Ram",Age:12,Sub:"Maths"},
+    {Name:"John",Age:13,Sub:"chemistry"},
+    {Name:"smith",Age:14,Sub:"botany"}];
+server.use(bodyparser.urlencoded({extended:true}))
+
+
+server.set("view engine","ejs");
+
+
+
+server.get("/",function(req,res){
+   res.render("login")
+})
+server.get("/home",function(req,res){
+    res.render("home",)
+})
+server.get("/aboutus",function(req,res){
+    res.render("aboutus")
+})
+server.get("/services",function(req,res){
+    res.render("services")
+})
+server.get("/login",function(req,res){
+    res.send("login page")
+})
+server.post("/validate",function(req,res){
+    var user=req.body.userid;
+    console.log(user)
+    var password=req.body.pass;
+    console.log(password)
+    if(user=="admin"&&password=="pass@123"){
+        res.render("home",{students:data})
+    }
+    else{
+        
+        //alert('User name or password invalid. please re-enter')
+        res.render("login")
+    }
+   
+})
+server.post("/check",function(req,res){
+    
+})
+server.listen(process.env.PORT||8000,function(){
+console.log("server started")
+}) 
